e50fc16108e699758eaf3fa5451e0f394977a75c
shinjjune <sjh8236@naver.com>
2020-04-06 20:42:37 +0900

    todotodotodo

--- app/src/main/java/com/shinjjune/Todo_list/MainActivity.kt
+++ app/src/main/java/com/shinjjune/Todo_list/MainActivity.kt
@@ -2,11 +2,52 @@ package com.shinjjune.Todo_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.shinjjune.Todo_list.databinding.ActivityMainBinding
+import com.shinjjune.Todo_list.databinding.ItemTodoBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val data = arrayListOf<Todo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        data.add(Todo("숙제",false))
+        data.add(Todo("청소",false))
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = TodoAdapter(data)
     }
+}
+
+data class Todo(val text: String, var isDone: Boolean)
+
+class TodoAdapter(private val myDataset: List<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(val binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): TodoAdapter.TodoViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(ItemTodoBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.binding.todoText.text = myDataset[position].text
+    }
+
+    override fun getItemCount() = myDataset.size
 }
