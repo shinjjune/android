b8fd3be2903a7348ea623df8cc43b0d6abd4ac23
shinjjune <sjh8236@naver.com>
2020-04-09 20:45:38 +0900

    Firebase login&logout

--- app/src/main/java/com/shinjjune/Todo_list/MainActivity.kt
+++ app/src/main/java/com/shinjjune/Todo_list/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.shinjjune.Todo_list
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Paint
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.LiveData
@@ -15,11 +15,16 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.shinjjune.Todo_list.databinding.ActivityMainBinding
 import com.shinjjune.Todo_list.databinding.ItemTodoBinding
 import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 1000;
+
     private lateinit var binding: ActivityMainBinding
 
     private val viewModel: MainViewModel by viewModels()
@@ -29,6 +34,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        // 로그인 안됨.
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            login()
+        }
+
 
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -52,6 +63,61 @@ class MainActivity : AppCompatActivity() {
             (binding.recyclerView.adapter as TodoAdapter).setData(it)
         })
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // 로그인 실패
+                finish()
+            }
+        }
+    }
+
+    fun login() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                login()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_log_out ->{
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
@@ -108,7 +174,7 @@ class TodoAdapter(
 
     override fun getItemCount() = myDataset.size
 
-    fun setData(newData : List<Todo>){
+    fun setData(newData: List<Todo>) {
         myDataset = newData
         notifyDataSetChanged()
     }
